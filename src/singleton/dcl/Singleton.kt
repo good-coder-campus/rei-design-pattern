857cf1ce8982bfc714296acec626104ec1a406c0
YunGyung Lee <yunklee@lguplus.co.kr>
2024-07-29 20:41:02 +0900

    dcl

---
+++ src/singleton/dcl/Singleton.kt
@@ -0,0 +1,16 @@
+package singleton.dcl
+
+class Singleton private constructor() {
+
+    companion object {
+        @Volatile
+        private var instance: Singleton? = null
+
+        fun getInstance(): Singleton {
+            return instance ?: synchronized(Singleton::class.java) {
+                instance ?: Singleton().also { instance = it }
+            }
+
+        }
+    }
+}
